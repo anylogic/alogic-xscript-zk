5a2cd9729182e5d204a90cce8fed0b64bba93454
lijun <lijun_ctg@189.cn>
2016-08-22 15:40:59 +0800

    补充offset参数检查

--- src/main/java/com/alogic/xscript/zk/ZKChildren.java
+++ src/main/java/com/alogic/xscript/zk/ZKChildren.java
@@ -13,6 +13,7 @@ import com.alogic.xscript.LogicletContext;
 import com.alogic.xscript.plugins.Segment;
 import com.alogic.xscript.zk.util.Path;
 import com.alogic.xscript.zk.util.ZooKeeperConnector;
+import com.anysoft.util.BaseException;
 import com.anysoft.util.Properties;
 import com.anysoft.util.PropertiesConstants;
 
@@ -33,42 +34,48 @@ public class ZKChildren extends Segment implements Watcher {
 	@Override
 	public void process(WatchedEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void configure(Properties p) {
 		// TODO Auto-generated method stub
 		super.configure(p);
-		
+
 		path = PropertiesConstants.getString(p, "path", "");
 		ignoreException = PropertiesConstants.getBoolean(p, "ignoreException", false);
 		tag = PropertiesConstants.getString(p, "tag", "item");
 		offset = PropertiesConstants.getInt(p, "offset", 0);
 		limit = PropertiesConstants.getInt(p, "limit", 15);
 	}
-	
+
 	@Override
 	protected void onExecute(Map<String, Object> root, Map<String, Object> current, LogicletContext ctx,
 			ExecuteWatcher watcher) {
 		// TODO Auto-generated method stub
+
 		ZooKeeperConnector conn = ctx.getObject("$zk-conn");
 		List<String> childrenList = conn.getChildren(new Path(path), this, ignoreException);
-		
-		//根据偏移量计算截取的子节点段
-		int startIndex = offset;
-		int endIndex = (offset + limit > childrenList.size()) ? childrenList.size() : (offset + limit);  
-		
-		//截出指定的子节点段
-		List<String> result = childrenList.subList(startIndex, endIndex);
 
-		//遍历子节点
-		if (result.size() > 0) {
-			for (String value : result) {
-				ctx.SetValue(tag, path + value);
-				super.onExecute(root, current, ctx, watcher);
+		if (offset < childrenList.size() && offset >= 0){
+			// 根据偏移量计算截取的子节点段
+			int startIndex = offset;
+			int endIndex = (offset + limit > childrenList.size()) ? childrenList.size() : (offset + limit);
+			// 截出指定的子节点段
+			List<String> result = childrenList.subList(startIndex, endIndex);
+
+			// 遍历子节点
+			if (result.size() > 0) {
+				for (String value : result) {
+					ctx.SetValue(tag, path + value);
+					super.onExecute(root, current, ctx, watcher);
+				}
 			}
+		}else {
+			throw new BaseException("zk-children", "illegal argument  offset");
 		}
+
+		
 	}
 
 }
